01c33d7fa68dc3e7f644c0a9be45acd4f7294550
Arne Bosien <info@arnebosien.de>
2022-08-28 16:12:59 +0200

    feat: add AbstractEntityStore

---
+++ packages/ng-utils/src/lib/services/store/abstract-entity-store.ts
@@ -0,0 +1,43 @@
+import { Directive } from '@angular/core';
+import { AbstractStore } from '@webskills/ng-utils';
+import { isUndefined } from '@webskills/ts-utils';
+
+@Directive()
+export abstract class AbstractEntityStore<
+  T extends { id: string }
+> extends AbstractStore<T[]> {
+  insertEntity(value: T): void {
+    this.update([...this.getInstant(), value]);
+  }
+
+  updateEntity(value: T): void {
+    if (isUndefined(value)) {
+      throw new Error('entity must be defined');
+    }
+
+    const idx = this.getInstant().findIndex((v) => v.id === value.id);
+
+    if (idx == -1) {
+      throw new Error('no such entity with id ' + value.id);
+    }
+
+    this.update([
+      ...this.getInstant().slice(0, idx),
+      value,
+      ...this.getInstant().slice(idx + 1),
+    ]);
+  }
+
+  deleteEntity(id: string): void {
+    const idx = this.getInstant().findIndex((v) => v.id === id);
+
+    if (idx == -1) {
+      throw new Error('no such entity with id ' + id);
+    }
+
+    this.update([
+      ...this.getInstant().slice(0, idx),
+      ...this.getInstant().slice(idx + 1),
+    ]);
+  }
+}
