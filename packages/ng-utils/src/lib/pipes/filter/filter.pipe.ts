fbb985693c7ba465c580884d95eb53446fee8276
Arne Bosien <info@arnebosien.de>
2022-10-13 20:32:49 +0200

    chore: documentation and typing of FilterPipe

--- packages/ng-utils/src/lib/pipes/filter/filter.pipe.ts
+++ packages/ng-utils/src/lib/pipes/filter/filter.pipe.ts
@@ -4,15 +4,20 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'filter',
 })
 export class FilterPipe<T> implements PipeTransform {
+  /**
+   * @param value list that will be filtered
+   * @param predicate function to test each value element
+   * @param param optional additional parameter that is passed into the predicate function
+   */
   transform(
     value: T[],
-    func: (v: T, data: any) => boolean,
-    data: any
+    predicate: (v: T, param: unknown) => boolean,
+    param: unknown
   ): T[] | undefined {
     if (!value) {
       return undefined;
     }
 
-    return value.filter((v) => func(v, data));
+    return value.filter((v) => predicate(v, param));
   }
 }
