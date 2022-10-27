bca86e3f05b119afd555c87d6cbac44bc5fd4a9c
Arne Bosien <info@arnebosien.de>
2022-10-28 00:08:19 +0200

    fix(ci): dtrack config (1)

---
+++ ci/generate-frontend-sbom.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+mkdir -p ci/.cache
+cd ci/.cache
+
+if ! [ -f syft ]; then
+    curl -L -o syft.tgz https://github.com/anchore/syft/releases/download/v0.59.0/syft_0.59.0_linux_amd64.tar.gz
+    tar -xzf syft.tgz
+fi
+
+./syft package ../../yarn.lock -o cyclonedx-xml > bom.xml
+
+curl -X "POST" "https://dtrack-api.colamda.de/api/v1/bom" \
+  -H 'Content-Type: multipart/form-data' \
+  -H "X-Api-Key: ${DTRACK_API_KEY}" \
+  -F "project=3cb485c7-3491-4b95-92b4-2341a20a5df8" \
+  -F "bom=@bom.xml"
