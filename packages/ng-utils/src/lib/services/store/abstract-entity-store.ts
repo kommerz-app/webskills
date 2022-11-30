7ee0e0012798081815125127076154cd50c36276
Arne Bosien <info@arnebosien.de>
2022-11-30 23:23:31 +0100

    feat: add upsert functionality to AbstractEntityStore

--- packages/ng-utils/src/lib/services/store/abstract-entity-store.ts
+++ packages/ng-utils/src/lib/services/store/abstract-entity-store.ts
@@ -2,12 +2,28 @@ import { Directive } from '@angular/core';
 import { AbstractStore } from './abstract-store';
 import { isUndefined } from '@webskills/ts-utils';
 
+type ID = string | null | undefined;
+
 @Directive()
 export abstract class AbstractEntityStore<
-  T extends { id?: string | null | undefined }
+  T extends { id?: ID }
 > extends AbstractStore<T[]> {
   insertEntity(value: T): void {
     this.update([...this.getInstant(), value]);
+  }
+
+  upsertEntity(value: T): void {
+    if (isUndefined(value)) {
+      throw new Error('entity must be defined');
+    }
+
+    const idx = this.getIdx(value.id);
+
+    if (idx == -1) {
+      this.insertEntity(value);
+    } else {
+      this.replaceValueAtIndex(value, idx);
+    }
   }
 
   updateEntity(value: T): void {
@@ -15,12 +31,16 @@ export abstract class AbstractEntityStore<
       throw new Error('entity must be defined');
     }
 
-    const idx = this.getInstant().findIndex((v) => v.id === value.id);
+    const idx = this.getIdx(value.id);
 
     if (idx == -1) {
       throw new Error('no such entity with id ' + value.id);
     }
 
+    this.replaceValueAtIndex(value, idx);
+  }
+
+  private replaceValueAtIndex(value: T, idx: number) {
     this.update([
       ...this.getInstant().slice(0, idx),
       value,
@@ -29,7 +49,7 @@ export abstract class AbstractEntityStore<
   }
 
   deleteEntity(id: string): void {
-    const idx = this.getInstant().findIndex((v) => v.id === id);
+    const idx = this.getIdx(id);
 
     if (idx == -1) {
       throw new Error('no such entity with id ' + id);
@@ -39,5 +59,9 @@ export abstract class AbstractEntityStore<
       ...this.getInstant().slice(0, idx),
       ...this.getInstant().slice(idx + 1),
     ]);
+  }
+
+  private getIdx(id: ID) {
+    return this.getInstant().findIndex((v) => v.id === id);
   }
 }
