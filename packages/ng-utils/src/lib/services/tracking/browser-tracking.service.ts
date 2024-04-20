d08069ed2acd6892c2e3fb3f4f6511bba93ad572
Arne Bosien <info@arnebosien.de>
2024-04-20 11:46:52 +0200

    fix: add global tracking of external links

--- packages/ng-utils/src/lib/services/tracking/browser-tracking.service.ts
+++ packages/ng-utils/src/lib/services/tracking/browser-tracking.service.ts
@@ -1,38 +1,69 @@
-import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { Inject, Injectable, OnDestroy, PLATFORM_ID } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { TrackingService } from './tracking.service';
 import { Breakpoints } from '../../breakpoints/breakpoints';
-import { forEachProp } from '@webskills/ts-utils';
+import { forEachProp, isNotBlank } from '@webskills/ts-utils';
 import { BreakpointObserver } from '@angular/cdk/layout';
-import { isPlatformBrowser } from '@angular/common';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
-export class BrowserTrackingService {
+export class BrowserTrackingService implements OnDestroy {
   private readonly isBrowser: boolean;
+  private readonly destroyed$ = new Subject<void>();
 
   constructor(
     private trackingService: TrackingService,
     private router: Router,
     private breakpointObserver: BreakpointObserver,
     @Inject(PLATFORM_ID) platformId: NonNullable<unknown>,
+    @Inject(DOCUMENT) private document: Document,
   ) {
     this.isBrowser = isPlatformBrowser(platformId);
   }
 
   init(): void {
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd && this.isBrowser) {
+    if (!this.isBrowser) {
+      return;
+    }
+
+    this.monitorAndTrackNavigationEvents();
+    this.trackExternalLinks();
+    this.trackUa();
+    this.trackBreakpoints();
+    this.trackingService.trackSession();
+  }
+
+  private monitorAndTrackNavigationEvents() {
+    this.router.events.pipe(takeUntil(this.destroyed$)).subscribe((event) => {
+      if (event instanceof NavigationEnd) {
         let baseUri = document.baseURI;
         baseUri = baseUri.substring(0, baseUri.length - 1);
         this.trackingService.trackVisit({ url: baseUri + event.url });
       }
     });
+  }
 
-    if (this.isBrowser) {
-      this.trackUa();
-      this.trackBreakpoints();
-      this.trackingService.trackSession();
-    }
+  private trackExternalLinks() {
+    this.document.addEventListener('click', (event) => {
+      if ((<Element>event.target)?.tagName === 'A') {
+        const link = (<HTMLAnchorElement>event.target).href;
+
+        if (
+          isNotBlank(link) &&
+          (link.startsWith('http://') || link.startsWith('https://'))
+        ) {
+          this.trackingService.trackInteraction({
+            event: 'externalLinkClicked',
+            component: 'browser-tracking',
+            data: {
+              url: link,
+            },
+          });
+        }
+      }
+    });
   }
 
   private trackBreakpoints(): void {
@@ -73,5 +104,10 @@ export class BrowserTrackingService {
       tz: Intl.DateTimeFormat().resolvedOptions().timeZone,
       c: navigator.cookieEnabled,
     });
+  }
+
+  ngOnDestroy() {
+    this.destroyed$.next();
+    this.destroyed$.complete();
   }
 }
