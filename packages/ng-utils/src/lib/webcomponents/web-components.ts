c6ef3cd0580f049f779633ec46e6327648b0c1c8
Arne Bosien <info@arnebosien.de>
2024-10-08 19:33:47 +0200

    fix: disable custom element on SSR

--- packages/ng-utils/src/lib/webcomponents/web-components.ts
+++ packages/ng-utils/src/lib/webcomponents/web-components.ts
@@ -1,12 +1,17 @@
-import { Injector, Type } from '@angular/core';
+import { Injector, PLATFORM_ID, Type } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
 import { isDefined } from '@webskills/ts-utils';
+import { isPlatformBrowser } from '@angular/common';
 
 export function registerWebComponent(
   tagName: string,
   component: Type<any>,
   injector: Injector,
 ): void {
+  if (!isPlatformBrowser(injector.get(PLATFORM_ID))) {
+    return;
+  }
+
   if (isDefined(customElements.get(tagName))) {
     return;
   }
