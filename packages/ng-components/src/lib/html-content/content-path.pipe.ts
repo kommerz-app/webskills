f83ca70357481f1c1c4aa2361828ce99dadc7d24
Arne Bosien <info@arnebosien.de>
2025-02-19 23:33:48 +0100

    ...

--- packages/ng-components/src/lib/html-content/content-path.pipe.ts
+++ packages/ng-components/src/lib/html-content/content-path.pipe.ts
@@ -1,6 +1,13 @@
-import { Inject, LOCALE_ID, Pipe, PipeTransform } from '@angular/core';
+import {
+  Inject,
+  LOCALE_ID,
+  Pipe,
+  PipeTransform,
+  PLATFORM_ID,
+} from '@angular/core';
 import { UrlSegment } from '@angular/router';
 import { transformPaths } from './content-path.utils';
+import { isPlatformBrowser } from '@angular/common';
 
 /**
  * transform relative path to load content from "assets/content/${locale}/..."
@@ -10,9 +17,16 @@ import { transformPaths } from './content-path.utils';
   standalone: true,
 })
 export class ContentPathPipe implements PipeTransform {
-  constructor(@Inject(LOCALE_ID) private readonly locale: string) {}
+  private readonly prefix: string;
+
+  constructor(
+    @Inject(LOCALE_ID) private readonly locale: string,
+    @Inject(PLATFORM_ID) platformId: NonNullable<unknown>,
+  ) {
+    this.prefix = isPlatformBrowser(platformId) ? '' : '/';
+  }
 
   transform(paths: UrlSegment[] | string | null | undefined): string | null {
-    return transformPaths(paths, this.locale);
+    return this.prefix + transformPaths(paths, this.locale);
   }
 }
