3675dc1adf04934f4a2c6f5026a27e4f2d58272a
Arne Bosien <info@arnebosien.de>
2025-09-04 12:32:04 +0200

    ...

--- packages/logging/jest.config.ts
+++ packages/logging/jest.config.ts
@@ -3,8 +3,14 @@ import type { Config } from 'jest';
 const config: Config = {
   clearMocks: true,
   coverageProvider: 'v8',
-  preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      { tsconfig: '<rootDir>/tsconfig.spec.json' },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
 };
 
 export default config;
